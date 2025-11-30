8721a1eb102be45896f0c735744339de14b2214f
AliEY0 <ali.elyousfi@outlook.be>
2025-11-30 13:38:41 +0100

    Add fibonacci and prime

---
+++ PrimeNumbersinC.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+bool isPrime(int n){
+    if(n < 2)
+        return false;
+    if(n == 2)
+        return true;
+
+    for(int i = 2; i * i <= n; i++){
+        if(n % i == 0)
+            return false;
+    }
+    
+    return true;
+}
+
+
+int main() {
+
+    clock_t begin = clock();
+    int totaal = 0;
+
+    const int N = 10000000;
+    for(int i = 0; i <= N; i++){
+        if(isPrime(i)){
+            printf("%d is een priem getal\n", i);
+            totaal++;
+        }
+    }
+
+    printf("\n");
+    printf("Er zijn in totaal %d priemgetallen onder %d\n", totaal, N);
+    clock_t end = clock();
+    double time_spent = (double)(end - begin)  / CLOCKS_PER_SEC;
+    printf("Totale tijd gespendeerd is: %f\n", time_spent);
+    return 0;
+}
+
+
