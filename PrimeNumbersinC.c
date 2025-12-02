b268a3157cfeac7f0c6039648b6da1735fa444ab
AliEY0 <ali.elyousfi@outlook.be>
2025-12-02 15:51:16 +0100

    update prime 20mil

--- PrimeNumbersinC.c
+++ PrimeNumbersinC.c
@@ -6,10 +6,10 @@
 bool isPrime(int n){
     if(n < 2)
         return false;
-    if(n == 2)
-        return true;
+    if(n % 2 == 0)
+        return false;
 
-    for(int i = 2; i * i <= n; i++){
+    for(int i = 3; i * i <= n; i+=2){
         if(n % i == 0)
             return false;
     }
@@ -21,9 +21,10 @@ bool isPrime(int n){
 int main() {
 
     clock_t begin = clock();
-    int totaal = 0;
+    int totaal = 1;
 
-    const int N = 10000000;
+    //const int N = 10000000;
+    const int N = 20000000;
     for(int i = 0; i <= N; i++){
         if(isPrime(i)){
             printf("%d is een priem getal\n", i);
