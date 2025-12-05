25ec643c4c038a8d1470d0b9eef3de3380ad3062
AliEY0 <ali.elyousfi@outlook.be>
2025-12-05 16:33:50 +0100

    add prime benchmark rust

---
+++ Benchmarks/PrimeRust/target/debug/PrimeRust.d
@@ -0,0 +1,1 @@
+/home/ali/SoftTrends/Programs/Benchmarks/PrimeRust/target/debug/PrimeRust: /home/ali/SoftTrends/Programs/Benchmarks/PrimeRust/src/main.rs
---
+++ Benchmarks/PrimeRust/target/debug/deps/PrimeRust-31dd3bce1ef926f9.d
@@ -0,0 +1,5 @@
+/home/ali/SoftTrends/Programs/Benchmarks/PrimeRust/target/debug/deps/PrimeRust-31dd3bce1ef926f9.d: src/main.rs
+
+/home/ali/SoftTrends/Programs/Benchmarks/PrimeRust/target/debug/deps/PrimeRust-31dd3bce1ef926f9: src/main.rs
+
+src/main.rs:
