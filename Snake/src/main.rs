f80e109cfba278d6f4bae20ba5fcde202909cbcc
AliEY0 <ali.elyousfi@outlook.be>
2025-12-06 14:03:43 +0100

    make it possible to change direction snake

--- Snake/src/main.rs
+++ Snake/src/main.rs
@@ -1,22 +1,32 @@
-mod board;
-mod snake;
+mod board; 
+mod snake; 
+use snake::Snake; 
+use board::Board; 
+fn main() { 
+    let size = 10; 
+    let mut b = Board::new(size, size); 
+    let mut s = Snake::new(size, size);
 
-use snake::Snake;
-
-
-use board::Board;
-
-
-
-
-fn main() {
-    let size = 10;    
-    let mut b = Board::new(size, size);
-    //let mut Board;
-    
-    let s = Snake::new(size, size);
 
     b.draw_snake(&s);
 
-    println!("Hello, world!");
+    println!();
+    s.change_dir(&mut b, 2);
+    b.draw_snake(&s);
+
+    println!();
+    s.change_dir(&mut b, 2);
+    b.draw_snake(&s);
+    
+    println!();
+    s.change_dir(&mut b, 3);
+    b.draw_snake(&s);
+    
+    println!();
+    s.change_dir(&mut b, 4);
+    b.draw_snake(&s);
+    
+    println!();
+    s.change_dir(&mut b, 1);
+    b.draw_snake(&s);
 }
--- Snake/src/snake.rs
+++ Snake/src/snake.rs
@@ -1,4 +1,4 @@
-
+use crate::board::Board;
 
 pub struct Point {
     pub row: usize,
@@ -7,7 +7,8 @@ pub struct Point {
 
 
 pub struct Snake{
-    pub body: Vec<Point>
+    pub body: Vec<Point>,
+    pub direction: usize,
 
 
 }
@@ -19,14 +20,55 @@ impl Snake{
         let row_start = board_rows_size/2;
         let col_start = board_cols_size / 2;
 
-        let length = 5;
+        let length = 3;
         for i in 0..length{
-            body.push(Point {row: row_start, col: col_start + i});
+            body.push(Point {row: row_start + i, col: col_start});
         }
 
-
-
-        Snake {body: body}
+        Snake {body: body, direction: 3}
     }
+
+pub fn change_dir(&mut self, board: &mut Board, key_dir: usize) {
+
+    let x = self.body.len();
+
+    board.board[self.body[x-1].row][self.body[x-1].col] = '.';
+
+    for i in (1..self.body.len()).rev() {
+        self.body[i].row = self.body[i - 1].row;
+        self.body[i].col = self.body[i - 1].col;
+    }
+
+    if self.direction == 1 && key_dir != 3 { 
+        self.direction = key_dir; 
+    }
+    if self.direction == 3 && key_dir != 1 {
+        self.direction = key_dir; 
+    }
+    if self.direction == 2 && key_dir != 4 { 
+        self.direction = key_dir; 
+    }
+    if self.direction == 4 && key_dir != 2 { 
+        self.direction = key_dir; 
+    }
+
+    if self.direction == 1 {
+        self.body[0].row -= 1; 
+    }
+    if self.direction == 2 {
+        self.body[0].col += 1;
+    }
+    if self.direction == 3 {
+        self.body[0].row += 1;
+    }
+    if self.direction == 4 {
+        self.body[0].col -= 1;
+    }
+
+    for point in &self.body {
+            board.board[point.row][point.col] = '*';
+        }
+    }
+
     
 }
